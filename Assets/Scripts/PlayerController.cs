a2c9e1a6d4e45a31d164ff9a5eb50f74da4981da
eppupiipari <beppi001@edu.xamk.fi>
2020-11-06 10:14:24 +0200

    created a player controller script that makes the player move and be able to place bombs.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public GameObject bomb;
+    public GameObject mine;
+    public float movementSpeed = 1;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(bomb,transform.position, transform.rotation);
+        }
+    }
+    void FixedUpdate()
+    {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, Input.GetAxisRaw("Vertical") * movementSpeed);
+            //sivuttain liikkuminen on nopeampaa. fix later!
+    }
+}
