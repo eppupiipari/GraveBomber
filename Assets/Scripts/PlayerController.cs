60197fb9b586b7d3da9686d64008a59712f4c33b
eppupiipari <beppi001@edu.xamk.fi>
2020-11-10 11:40:07 +0200

    finished the player walk animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     public GameObject bomb;
     public GameObject mine;
+    public Animator playerAnimator;
+
     public float movementSpeed = 1;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
     void Update()
     {
@@ -18,10 +22,21 @@ public class PlayerController : MonoBehaviour
         {
             Instantiate(bomb,transform.position, transform.rotation);
         }
+        if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            playerAnimator.SetTrigger("IsMoving");
+        }
+
     }
     void FixedUpdate()
     {
+        if (Input.GetButton("Horizontal") && Input.GetButton("Vertical"))
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed / 1.5f, Input.GetAxisRaw("Vertical") * movementSpeed / 1.5f);
+        }
+        else
+        {
             rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, Input.GetAxisRaw("Vertical") * movementSpeed);
-            //sivuttain liikkuminen on nopeampaa. fix later!
+        }
     }
 }
